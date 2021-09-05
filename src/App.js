7f3c93de78064a4f252a95d37229f1007fa34f4b
ariannabrunell <83625264+ariannabrunell@users.noreply.github.com>
2021-09-05 16:38:50 -0500

    Updated current icon url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App(props) {
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
